1964a16e6269c71e6cb53af643fab1e24cb3f02e
Sixl-Daniel <sixl.daniel@googlemail.com>
2018-05-11 11:14:19 +0200

    test: Adding test for initial entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -104,12 +104,17 @@ $(function() {
 
     describe('Initial Entries', function () {
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        beforeEach(function (done) {
+            const randomChannel = Math.floor(Math.random() * allFeeds.length);
+            loadFeed(randomChannel, function () {
+                done();
+            });
+        });
+
+        it('have loaded and contain at least one element', function (done) {
+            expect($('.feed .entry').length).not.toBe(0);
+            done();
+        });
 
     });
 
