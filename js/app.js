7745ba4e6ac22d45013acbbe96a7e5bbaee9c7a5
Sixl-Daniel <sixl.daniel@googlemail.com>
2018-05-11 17:57:08 +0200

    feat: Adding summary and fading effect when switching feed

--- js/app.js
+++ js/app.js
@@ -33,10 +33,6 @@ var allFeeds = [
         url: 'http://feeds2.feedburner.com/tympanus'
     },
     {
-        name: 'How-To Geek',
-        url: 'http://feeds.feedburner.com/HowToGeek'
-    },
-    {
         name: 'Smashingmagazine',
         url: 'https://www.smashingmagazine.com/feed/'
     },
@@ -45,17 +41,9 @@ var allFeeds = [
         url: 'https://tutorialzine.com/feed/'
     },
     {
-        name: 'JavaScript Weekly',
-        url: 'https://javascriptweekly.com/rss/'
-    },
-    {
         name: 'Web Design Weekly',
         url: 'http://web-design-weekly.com/feed/'
-    },
-    {
-        name: 'David Walsh Blog',
-        url: 'https://davidwalsh.name/feed'
-    },
+    }
 ];
 
 /* This function starts up our application. The Google Feed
@@ -93,20 +81,30 @@ function init() {
                      entryTemplate = Handlebars.compile($('.tpl-entry').html());
 
                  title.html(feedName);   // Set the header text
-                 container.empty();      // Empty out all previous entries
 
-                 /* Loop through the entries we just loaded via the Google
-                  * Feed Reader API. We'll then parse that entry against the
-                  * entryTemplate (created above using Handlebars) and append
-                  * the resulting HTML to the list of entries on the page.
-                  */
-                 entries.forEach(function(entry) {
-                     container.append(entryTemplate(entry));
+                 container.fadeOut(300, function () {
+                        
+                        container.empty();
+
+                        /* Loop through the entries we just loaded via the Google
+                         * Feed Reader API. We'll then parse that entry against the
+                         * entryTemplate (created above using Handlebars) and append
+                         * the resulting HTML to the list of entries on the page.
+                         */
+
+                        entries.forEach(function (entry) {
+                            container.append(entryTemplate(entry));
+                        });
+
+                        container.delay(300).fadeIn(600);
+
+                        if (cb) {
+                            cb();
+                        }
+
                  });
 
-                 if (cb) {
-                     cb();
-                 }
+
                },
        error: function (result, status, err){
                  //run only the callback without attempting to parse result due to error
