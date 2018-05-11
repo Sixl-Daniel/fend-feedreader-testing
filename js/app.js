f2b4f5df004afbd854e31335b474e8ffa843fab6
Sixl-Daniel <sixl.daniel@googlemail.com>
2018-05-11 10:57:25 +0200

    test: Adding tests for RSS feeds and the off canvas menu

--- js/app.js
+++ js/app.js
@@ -11,15 +11,26 @@ var allFeeds = [
     {
         name: 'Udacity Blog',
         url: 'http://blog.udacity.com/feed'
-    }, {
+    },
+    {
         name: 'CSS Tricks',
         url: 'http://feeds.feedburner.com/CssTricks'
-    }, {
+    },
+    {
         name: 'HTML5 Rocks',
         url: 'http://feeds.feedburner.com/html5rocks'
-    }, {
+    },
+    {
         name: 'Linear Digressions',
         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
+    },
+    {
+        name: 'Digital Inspiration Technology Blog',
+        url: 'http://feeds.feedburner.com/labnol'
+    },
+    {
+        name: 'How-To Geek',
+        url: 'http://feeds.feedburner.com/HowToGeek'
     }
 ];
 
